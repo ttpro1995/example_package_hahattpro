2ca695672f02ceba8bedc17cfd90a2cec3afec8d
Thai Thien <thient@vng.com.vn>
2023-09-25 16:18:29 +0700

    0.0.8dev time 1

--- src/example_package_hahattpro/__init__.py
+++ src/example_package_hahattpro/__init__.py
@@ -1,3 +1,4 @@
 from .example import add_one
 from .the_template_reader import read_template
 from .the_jinja2 import render_mytemplate
+from .dev_class import DevClass
---
+++ src/example_package_hahattpro/dev_class.py
@@ -0,0 +1,6 @@
+class DevClass:
+    def __init__(self) -> None:
+        self.mes = "0.0.8dev time 1"
+
+    def get_message(self):
+        return self.mes
